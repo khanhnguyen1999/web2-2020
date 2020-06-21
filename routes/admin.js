5cae23f4fc61342310235ad3b90a2b8f1c77e2c1
Daniel <44046364+Danieldot29@users.noreply.github.com>
2020-06-21 13:05:58 +0700

    need assistance

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const Account = require('../services/account');
 const asyncHandler = require('express-async-handler');
 const User = require('../services/user');
 const { Sequelize } = require('sequelize');
+const Op = Sequelize.Op;
 
 router.get('/', asyncHandler(async (req, res) => {
     const account = await Account.findAccountrByUserId(req.currentUser.id);
@@ -18,32 +19,47 @@ router.get('/users', asyncHandler(async (req, res) => {
     const users = [];
     res.render('./pages/admin/users', { users });
 }));
-
+// #########################################################
 router.post('/users', asyncHandler(async (req, res) => {
     const { search } = req.body;
 
-    const users = await Account.findAll({
+    console.log('#######');
+
+    const re = [];
+    const result = await Account.findAll({
         where: {
+            accountNumber: {
+                [ Op.like ]: search,
+            },
             role: 'user',
         },
-        where: {
-            accountNumber: search,
-        },
-    }).then((data) => {
-        data.forEach(async (user) => {
-            const u = await User.findOne({
+        // where: {
+        // },
+    }).then(async (data) => {
+        if (data != null) {
+            const user = await User.findOne({
                 where: {
-                    id: user.userId,
+                    id: data[ 0 ].userId,
                 }
             });
-        });
+
+            Object.assign(user, data[ 0 ]);
+
+            // console.log(Object.assign(data[0], user));
+            console.log(user);
+
+            // re.push(d);
+            // console.log(re);
+            return re;
+        }
     }).catch((err) => {
         console.log(err);
     });
 
-    console.log(`>>>> ${users}`);
+    console.log(`>>>> ${result}`);
 
-    res.render('./pages/admin/users', { users });
+    res.render('./pages/admin/users', { users: result });
 }));
+// #########################################################
 
 module.exports = router;
