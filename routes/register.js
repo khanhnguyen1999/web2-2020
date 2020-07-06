d785e65279b2b0184d3659329db2b71943dd8bd0
mac <mac@macs-MacBook-Air.local>
2020-07-06 14:52:54 +0700

    fix register

--- routes/register.js
+++ routes/register.js
@@ -42,21 +42,34 @@ router.post('/', [
     if (!errors.isEmpty()) {
         return res.status(422).render('pages/register', { errors: errors.array() });
     }
-  
+
+    // Fix: Adding user's id into account.user_id
     await User.create({
         username: req.body.username,
         email: req.body.email,
         displayName: req.body.displayName,
         password: User.hashPassword(req.body.password),
+    }).then(async (user) => {
+        await Account.create({
+            accountNumber: 970460 + Math.floor(Math.random() * 1000) + 1, // Wrong algo
+            balance: 100000,
+            currencyUnit: 'VND',
+            role: 'user',
+            status: false,
+            limit: 0,
+            userId: user.id,
+        });
     });
-    await Account.create({
-        accountNumber: 970460 + Math.floor(Math.random() * 1000) + 1,
-        balance: 100000,
-        currencyUnit: 'VND',
-        status: false,
-        limit: 0,
-    });
-  
+
+    // await Account.create({
+    //     accountNumber: 970460 + Math.floor(Math.random() * 1000) + 1, // Wrong algo
+    //     balance: 100000,
+    //     currencyUnit: 'VND',
+    //     role: 'user',
+    //     status: false,
+    //     limit: 0,
+    // });
+
     // await Email.send(user.email,'Mã kích hoạt tài khoản',`link activate của bạn là : ${process.env.BASE_URL}/login/${user.id}/${user.token}`)
     res.redirect('/');
 }));
