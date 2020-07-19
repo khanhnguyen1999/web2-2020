3a837a0cdaf70fb0aefd2d3dc23fe3cdccf9fe9a
daniel-ducbui <ducbui2999@gmail.com>
2020-07-19 17:26:36 +0700

    trying to route 2

---
+++ client/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./views/partials/navbar";
+
+import Login from "./views/pages/login";
+import Home from "./views/pages/home";
+import Transaction from "./views/pages/transaction";
+import Profile from "./views/pages/profile";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Navbar />
+                <Router>
+                    <Switch>
+                        <Route path="/" exact component={Home}>
+                            <Home />
+                        </Route>
+                        <Route path="/transaction" component={Transaction}>
+                            <Transaction />
+                        </Route>
+                        <Route path="/profile" component={Profile}>
+                            <Profile />
+                        </Route>
+                        <Route path="/login" component={Login}>
+                            <Login />
+                        </Route>
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
+}
+
+export default App;
