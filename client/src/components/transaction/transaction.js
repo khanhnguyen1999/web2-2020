fdc2b889d3ca42717080fa45926f7fddd8491632
unknown <chi2caithoi@gmail.com>
2020-08-16 15:06:05 +0700

    update heroku

--- client/src/components/transaction/transaction.js
+++ client/src/components/transaction/transaction.js
@@ -191,7 +191,7 @@ function transaction(props) {
                           </div>
                           <div className="row justify-content-center">
                             <div className="col-12">
-                              <p className="class-text">
+                              <p style={{color:"white"}} className="class-text">
                                 {user.displayName}
                               </p>
                             </div>
@@ -207,7 +207,7 @@ function transaction(props) {
                           </div>
                           <div className="row justify-content-center">
                             <div className="col-12">
-                              <p className="class-text">
+                              <p style={{color:"white"}} className="class-text">
                                 {account?account.accountNumber:''}
                               </p>
                             </div>
@@ -216,16 +216,16 @@ function transaction(props) {
                         <div className="col-lg-4 col-md-12">
                           <div className="row justify-content-center">
                             <div className="col-12">
-                              <p className="card-text">
+                              <p  className="card-text">
                                 <b>Balance: </b>
                               </p>
                             </div>
                           </div>
                           <div className="row justify-content-center">
                             <div className="col-12">
-                              <p className="class-text">
+                              <p style={{color:"white"}} className="class-text">
                               {account?FC.inMoney(account.balance):''}
-                                <small className="muted-text">&nbsp;  VND</small>
+                                <small style={{color:"yellow"}} className="muted-text">&nbsp;  VND</small>
                               </p>
                             </div>
                           </div>
